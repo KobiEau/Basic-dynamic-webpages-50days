63ab7e0cb14344fc396c0e50e23e0b3dd15bdee1
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2024-07-12 15:02:36 +0000

    Fixed code conflict

--- Day 1 Expanding Cards/script.js
+++ Day 1 Expanding Cards/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
@@ -12,19 +11,4 @@ function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
-=======
-const panels = document.querySelectorAll('.panel');
-
-panels.forEach((panel) => {
- panel.addEventListener('click',() =>{
-    removeActiveClasses();
-    panel.classList.toggle('active')
- })
-});
-
-function removeActiveClasses(){
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
->>>>>>> c80b7c9150b6589211a715506c55698279865305
 }
